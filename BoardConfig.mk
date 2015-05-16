f86be21685cb3665e05c2618f60d44771f6d802d
Men_in_black007 <sanyam.53jain@gmail.com>
2015-05-16 06:51:55 -0400

    Move fstab.qcom to rootdir and Add missing changes
    Signed-off-by: Men_in_black007 <sanyam.53jain@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,7 +95,7 @@ BOARD_USES_QCOM_HARDWARE := true
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/sony/e1/ramdisk/fstab.qcom
+TARGET_RECOVERY_FSTAB := device/sony/e1/rootdir/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 TARGET_USERIMAGES_USE_EXT4 := true
